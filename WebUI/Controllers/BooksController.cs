6da06baf06d0e09800b139b27210ad20df70f083
Daniil <hedgehog147@gmail.com>
2016-08-16 17:32:57 +0300

    bookstore

---
+++ WebUI/Controllers/BooksController.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class BooksController : Controller
+    {
+        private IBookRepository repository;
+        //кол-во товаров на странице
+        public int pageSize = 4;
+
+        public BooksController(IBookRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List(int page = 1)
+        {
+            BooksListViewModel model = new BooksListViewModel
+            {
+                Books = repository.Books
+                .OrderBy(book => book.BookId)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize), 
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = pageSize,
+                    TotalItems = repository.Books.Count()
+                }
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ WebUI/Models/PagingInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebUI.Models
+{
+    public class PagingInfo
+    {
+        public int TotalItems { get; set; } //общее кол-во книг
+        public int ItemsPerPage { get; set; } //кол-во книг на странице
+        public int CurrentPage { get; set; } //номер текущей страницы
+
+        public int TotalPages                //общее кол-во страниц
+        {
+            get { return (int) Math.Ceiling((decimal) TotalItems/ItemsPerPage); }
+        }
+    }
+}
